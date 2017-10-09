9fccab2698bd2f28ad72ac425c79a0f106213de8
IlievskiV <ilievski.vladimir@live.com>
2017-10-09 14:12:32 +0200

    Initial code for the DQN agent

--- core/agent/dqn_agent.py
+++ core/agent/dqn_agent.py
@@ -24,13 +24,23 @@ class GOChatBotDQNAgent(DQNAgent):
     
     From [AbstractDQNAgent] class:
         - nb_actions: the number of all possible actions.
-        - memory:
+        - memory (`Memory` instance): the type of buffer the agent will use. For example: SequentialMemory.
         - gamma: the discount reward factor. Default is 0.99, the agent remembers everything.
         - batch_size: the number of memories to replay in one training epoch. Default is 32.
         - nb_steps_warmup: the number of steps needed to warm up and fill the memory replay buffer. Default is 1000.
-        - train_interval:
-        - memory_interval:
-        - target_model_update: after how many steps, the target DQN gets updated. Default is 
+        - train_interval: no idea
+        - memory_interval: no idea
+        - target_model_update: after how many steps, the target DQN gets updated. Default is 10 000.
+        - delta_range: no idea. Default is None. This variable is deprecated.
+        - delta_clip: no idea. Default is Inf.
+        - custom_model_objects: no idea.
+    
+    From [DQNAgent] class:
+        - policy (`Policy` instance): The policy that the agent follows. Default is None.
+        - test_policy (`Policy` instance): The policy that the agent follows during testing. Default is None.
+        - enable_double_dqn: To enable the Double DQN technique. By default is on.
+        - enable_dueling_network: To enble the Dueling DQN technique. Be default is off.
+        - dueling_type: If the dueling is on, what type of it. The default is average.
     
     """
 
