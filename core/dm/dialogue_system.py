6a8182f622785bc3ab836b17f55c0fff8003bf42
IlievskiV <ilievski.vladimir@live.com>
2017-10-19 21:28:10 +0200

    Change docs

--- core/dm/dialogue_system.py
+++ core/dm/dialogue_system.py
@@ -13,16 +13,16 @@ class GODialogSys():
     """
     The GO Dialogue System mediates the interaction between the environment and the agent.
     
-    # Class members:
+    # Class members: 
     
-        - agent: the type of conversational agent. Default is None (temporarily).
-        - env: the environment with which the agent and user interact. Default is None (temporarily).
-        - act_set: the set of all intents used in the dialogue.
-        - slot_set: the set of all slots used in the dialogue.
-        - kb_path: path to any knowledge base
-        - agt_feasible_actions: list of templates described as dictionaries, corresponding to each action the agent might take
+        - `agent`: the type of conversational agent. Default is None (temporarily).
+        - `env`: the environment with which the agent and user interact. Default is None (temporarily).
+        - `act_set`: the set of all intents used in the dialogue.
+        - `slot_set`: the set of all slots used in the dialogue.
+        - `kb_path`: path to any knowledge base
+        - `agt_feasible_actions`: list of templates described as dictionaries, corresponding to each action the agent might take
                                 (dict to be specified)
-        - max_nb_turns: the maximal number of dialogue turns
+        - `max_nb_turns`: the maximal number of dialogue turns
     
     """
 
--- docs/sources/dm/overview.md
+++ docs/sources/dm/overview.md
@@ -8,16 +8,16 @@ core.dm.dialogue_system.GODialogSys(act_set=None, slot_set=None, agt_feasible_ac
 
 The GO Dialogue System mediates the interaction between the environment and the agent.
 
-__Class members:__
+__Class members: __
 
 
-- agent: the type of conversational agent. Default is None (temporarily).
-- env: the environment with which the agent and user interact. Default is None (temporarily).
-- act_set: the set of all intents used in the dialogue.
-- slot_set: the set of all slots used in the dialogue.
-- kb_path: path to any knowledge base
-- agt_feasible_actions: list of templates described as dictionaries, corresponding to each action the agent might take
+- `agent`: the type of conversational agent. Default is None (temporarily).
+- `env`: the environment with which the agent and user interact. Default is None (temporarily).
+- `act_set`: the set of all intents used in the dialogue.
+- `slot_set`: the set of all slots used in the dialogue.
+- `kb_path`: path to any knowledge base
+- `agt_feasible_actions`: list of templates described as dictionaries, corresponding to each action the agent might take
 			(dict to be specified)
-- max_nb_turns: the maximal number of dialogue turns
+- `max_nb_turns`: the maximal number of dialogue turns
 
 
